387a97815bb642b32392c5b4b281ce41970fb75a
Olua <a8rill@mail.ru>
2023-02-16 20:36:41 +0300

    added second_digit project

---
+++ second_digit/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите  трехзначное положительное число:");
+string number = Convert.ToString(Console.ReadLine());
+int length = number.Length;
+int number_int = Int32.Parse(number);
+if (number_int <= 0)
+{
+    Console.WriteLine("Вы ввели отрицательное  число");
+}
+else if (length != 3)
+{
+    Console.WriteLine("Вы ввели не  трехзначное  число");
+}
+
+else if (length == 3)
+{
+
+    int result = (number_int / 10) % 10;
+    Console.WriteLine($"Вторая  цифра числа {number_int}: {result}");
+}
+
